992694db34500a219992f03dc8fdf6c930aa279f
Bryan Robbins <bryantrobbins@gmail.com>
2015-04-04 15:41:22 -0400

    Add logging to controller, set base to DEBUG

--- src/main/groovy/com/btr3/demo/controllers/HelloController.groovy
+++ src/main/groovy/com/btr3/demo/controllers/HelloController.groovy
@@ -1,6 +1,8 @@
 package com.btr3.demo.controllers
 
 import com.btr3.demo.model.Greeting
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.security.access.annotation.Secured
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal
@@ -12,6 +14,7 @@ import java.util.concurrent.atomic.AtomicLong
 @RestController
 public class HelloController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class)
     private static final String template = "Hello, %s, you fantastic %s!";
     private final AtomicLong counter = new AtomicLong();
 
@@ -22,12 +25,14 @@ public class HelloController {
 
     @RequestMapping("/greet/any")
     public Greeting sayHello(@AuthenticationPrincipal UserDetails customUser) {
+        logger.info("Replying with a generic greeting")
         return new Greeting(counter.incrementAndGet(), String.format(template, customUser.getUsername(), "employee"));
     }
 
     @Secured("ROLE_DEVELOPERS")
     @RequestMapping("/greet/dev")
     public Greeting sayHelloDev(@AuthenticationPrincipal UserDetails customUser) {
+        logger.info("Replying with a developer-specific greeting")
         return new Greeting(counter.incrementAndGet(), String.format(template, customUser.getUsername(), "developer"));
     }
 
